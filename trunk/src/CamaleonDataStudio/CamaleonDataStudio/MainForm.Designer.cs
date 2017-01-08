49d26a9724a40ae823fc261c813ff47668c3f7ca
Juan José <juanjose.montesdeocaarbos@gmail.com>
2017-01-08 00:18:34 -0300

    Add main menu and exit menu

--- trunk/src/CamaleonDataStudio/CamaleonDataStudio/MainForm.Designer.cs
+++ trunk/src/CamaleonDataStudio/CamaleonDataStudio/MainForm.Designer.cs
@@ -28,21 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripMain.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(284, 24);
+            this.menuStripMain.TabIndex = 1;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.fileToolStripMenuItem.Text = "&Archivo";
+            // 
+            // exitFileToolStripMenuItem
+            // 
+            this.exitFileToolStripMenuItem.Name = "exitFileToolStripMenuItem";
+            this.exitFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitFileToolStripMenuItem.Text = "&Salir";
+            this.exitFileToolStripMenuItem.Click += new System.EventHandler(this.exitFileToolStripMenuItem_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.menuStripMain);
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStripMain;
             this.Name = "MainForm";
             this.Text = "Camaleon DataStudio";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitFileToolStripMenuItem;
     }
 }
 
--- trunk/src/CamaleonDataStudio/CamaleonDataStudio/MainForm.cs
+++ trunk/src/CamaleonDataStudio/CamaleonDataStudio/MainForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CamaleonDataStudio
@@ -15,6 +8,11 @@ namespace CamaleonDataStudio
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void exitFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
