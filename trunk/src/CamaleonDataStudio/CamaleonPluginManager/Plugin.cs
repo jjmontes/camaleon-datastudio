9c20039b66743e42bd50362e3dbe360e96704bd6
Juan José <juanjose.montesdeocaarbos@gmail.com>
2017-02-13 13:23:05 -0300

    PluginManager load dlls with IConnection implemented

--- trunk/src/CamaleonDataStudio/CamaleonPluginManager/Plugin.cs
+++ trunk/src/CamaleonDataStudio/CamaleonPluginManager/Plugin.cs
@@ -1,36 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ConnectionCamaleonDataStudio;
+using System;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace CamaleonPluginManager
 {
     public class Plugin
     {
-        private const string tagNameStart = "<name>";
-        private const string tagNameEnd = "</name>";
+        private const string tagName = "name";
+        private const string tagDll = "dll";
 
         public Plugin(string file)
         {
             var xml = File.ReadAllText(file);
-            var start = xml.IndexOf(tagNameStart, StringComparison.InvariantCultureIgnoreCase);
-            if (start == -1) throw new PluginMissingConfigurationException("name");
-            start += tagNameStart.Length;
-            var end = xml.IndexOf(tagNameEnd, start, StringComparison.InvariantCultureIgnoreCase);
-
-            Name = xml.Substring(start, end - start);
+            Name = GetTagContent(ref xml, tagName);
             Path = file;
+            Folder = (new FileInfo(file)).DirectoryName;
+            if (!Folder.EndsWith("\\")) Folder += "\\";
+            Dll = Folder + GetTagContent(ref xml, tagDll);
         }
 
         public string Name { get; private set; }
 
         public string Path { get; private set; }
 
+        public string Folder { get; private set; }
+
+        private string Dll { get; set; }
+
         public override string ToString()
         {
             return Name;
+        }
+
+        public IConnection Load()
+        {
+            IConnection typeIConnection = null;
+
+            var assembly = Assembly.LoadFile(Dll);
+            var types = assembly.GetExportedTypes();
+            foreach (var type in types)
+            {
+                if (type.GetInterfaces().Contains(typeof(IConnection)))
+                {
+                    typeIConnection = (IConnection)Activator.CreateInstance(type);
+                    break;
+                }
+            }
+
+            return typeIConnection;
+        }
+
+        private string GetTagContent(ref string xml, string tag)
+        {
+            var tagStart = string.Format("<{0}>", tag);
+            var tagEnd = string.Format("</{0}>", tag);
+
+            var start = xml.IndexOf(tagStart, StringComparison.InvariantCultureIgnoreCase);
+            if (start == -1) throw new PluginMissingConfigurationException(tag);
+            start += tagStart.Length;
+            var end = xml.IndexOf(tagEnd, start, StringComparison.InvariantCultureIgnoreCase);
+
+            return xml.Substring(start, end - start);
         }
     }
 }
